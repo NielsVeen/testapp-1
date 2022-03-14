bc58901f18e19474bb8062c2f972b1d84a6c210c
NielsVeen <41052701+NielsVeen@users.noreply.github.com>
2022-03-14 21:33:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,json
 
 app = Flask(__name__)
 
@@ -8,8 +8,9 @@ def home():
 
 @app.route('/post',methods=['POST'])
 def post():
-    data = request.form.get('text')
-    return jsonify({'your_message': data})
+    data = request.json
+    message = data['text']
+    return jsonify({'your_message': message})
 
 if __name__ == '__main__':
     app.run(threaded=True,port=5000)
